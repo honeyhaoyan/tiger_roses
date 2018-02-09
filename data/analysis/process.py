d50898039eb025aa4a6bf9ea8efebfd0598fe290
New-bottle <775712558@qq.com>
2018-02-09 12:56:13 +0800

    csv example

---
+++ data/analysis/process.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pickle
+import csv
+
+data_file = '../data/data.csv'
+
+X = []
+with open(data_file, 'rb') as f:
+	csv_reader = csv.DictReader(f)
+	for row in csv_reader:
+		X.append([row['MSN'], row['StateCode'], int(row['Year']), float(row['Data'])])
+
+with open('../data/data.pk1', 'wb') as f:
+	pickle.dump(X, f)
+
+descriptions = np.array(X)
+print (descriptions[:,2])
