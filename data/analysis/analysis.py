b1983414df660cf7e3e93db64ba9cf35fc8bfec6
WantonWind <875357936@qq.com>
2018-02-10 20:32:19 +0800

    seds added

--- data/analysis/analysis.py
+++ data/analysis/analysis.py
@@ -91,64 +91,64 @@ def plot_stacked_bar_chart_by_matrix(
 	plt.suptitle(title+"="+','.join(legends))
 	fig.savefig(fig_path+title+"="+','.join(legends)+'-bar_chart.png')
 
-# def plot_stacked_bar_chart(
-# 	change_type, c_types, 
-# 	stable_type = ['sector', 'Unit'], s_type = ['TC','Billion Btu'],
-# 	width = 0.5, xlabel='Year', ylabel=None, ylim='scalable', title = ""):
-# 	'''
-# 	change_type : type name used to plot stacked graph
-# 	c_types     : energy_types want to stack
-# 	stable_type : type name which are same between msncodes
-# 	s_type      : stable_type values
-# 	width       : width of bar
-# 	xlabel      : xlabel of graph
-# 	ylabel      : ylabel of graph
-# 	'''
-# 	prepare()
-# 	global X
-# 	global tag
-# 	global state
-# 	fig = plt.figure(figsize=(16,10))
-# 	num_of_types = len(c_types)
-# 	data = np.zeros([4, num_of_types, 50])
-# 	min_v = 1e20
-# 	max_v = -1e20
-# 	for i in range(num_of_types):
-# 		for row in X:
-# 			info = tag[row['MSN']]
-# 			flag = True
-# 			for j in range(len(stable_type)):
-# 				if info[stable_type[j]] != s_type[j]:
-# 					flag = False
-# 					break
-# 			if info[change_type] == c_types[i] and flag == True:
-# 				data[state[row['StateCode']]][i][int(row['Year']) - start_year] += float(row['Data'])
-# 	if ylim == 'same':
-# 		for i in range(4):
-# 			for k in range(50):
-# 				s = 0
-# 				for j in range(num_of_types):
-# 						s = s + data[i][j][k]
-# 				min_v = min(min_v, s)
-# 				max_v = max(max_v, s)
-# 	ind = np.arange(1960, 2010, 1)
-# 	legends = ['Arizona','California','New Mexico', 'Texas']
-# 	last = np.zeros([4, num_of_types, 50])
-# 	for i in range(4):
-# 		plt.subplot(2,2,i+1)
-# 		plt.bar(ind, data[i][0], width)
-# 		last[i][0] = data[i][0]
-# 		for j in range(1, num_of_types):
-# 			plt.bar(ind, data[i][j], width,bottom=last[i][j-1])
-# 			last[i][j] = last[i][j-1]+data[i][j]
-# 		plt.legend(c_types)
-# 		if ylim == 'same':
-# 			plt.ylim((min_v, max_v))
-# #		fig.text(0.30,0.50,legends[i],ha='center')
-# 		plt.title(legends[i], va='bottom')
-# #	plt.show()
-# 	plt.suptitle(title+"="+','.join(c_types))
-# 	fig.savefig(fig_path+title+"="+','.join(c_types)+'-bar_chart.png')
+def plot_stacked_bar_chart(
+	change_type, c_types, 
+	stable_type = ['sector', 'Unit'], s_type = ['TC','Billion Btu'],
+	width = 0.5, xlabel='Year', ylabel=None, ylim='scalable', title = ""):
+	'''
+	change_type : type name used to plot stacked graph
+	c_types     : energy_types want to stack
+	stable_type : type name which are same between msncodes
+	s_type      : stable_type values
+	width       : width of bar
+	xlabel      : xlabel of graph
+	ylabel      : ylabel of graph
+	'''
+	prepare()
+	global X
+	global tag
+	global state
+	fig = plt.figure(figsize=(16,10))
+	num_of_types = len(c_types)
+	data = np.zeros([4, num_of_types, 50])
+	min_v = 1e20
+	max_v = -1e20
+	for i in range(num_of_types):
+		for row in X:
+			info = tag[row['MSN']]
+			flag = True
+			for j in range(len(stable_type)):
+				if info[stable_type[j]] != s_type[j]:
+					flag = False
+					break
+			if info[change_type] == c_types[i] and flag == True:
+				data[state[row['StateCode']]][i][int(row['Year']) - start_year] += float(row['Data'])
+	if ylim == 'same':
+		for i in range(4):
+			for k in range(50):
+				s = 0
+				for j in range(num_of_types):
+						s = s + data[i][j][k]
+				min_v = min(min_v, s)
+				max_v = max(max_v, s)
+	ind = np.arange(1960, 2010, 1)
+	legends = ['Arizona','California','New Mexico', 'Texas']
+	last = np.zeros([4, num_of_types, 50])
+	for i in range(4):
+		plt.subplot(2,2,i+1)
+		plt.bar(ind, data[i][0], width)
+		last[i][0] = data[i][0]
+		for j in range(1, num_of_types):
+			plt.bar(ind, data[i][j], width,bottom=last[i][j-1])
+			last[i][j] = last[i][j-1]+data[i][j]
+		plt.legend(c_types)
+		if ylim == 'same':
+			plt.ylim((min_v, max_v))
+#		fig.text(0.30,0.50,legends[i],ha='center')
+		plt.title(legends[i], va='bottom')
+#	plt.show()
+	plt.suptitle(title+"="+','.join(c_types))
+	fig.savefig(fig_path+title+"="+','.join(c_types)+'-bar_chart.png')
 
 
 def plot_percentage_stacked_bar_chart_by_matrix(
@@ -161,7 +161,6 @@ def plot_percentage_stacked_bar_chart_by_matrix(
 	last = np.zeros([4, num_of_types, 50])
 	for i in range(4):
 		sm = np.sum(data[i], axis=0)
-		plt.subplot(2,2,i+1)
 		plt.bar(ind, data[i][0]*100.0/sm, width)
 		last[i][0] = data[i][0]*100.0/sm
 		for j in range(1, num_of_types):
@@ -174,53 +173,53 @@ def plot_percentage_stacked_bar_chart_by_matrix(
 	fig.savefig(fig_path+title+"="+','.join(legends)+'-percentage.png')
 #	plt.show()
 
-# def plot_percentage_stacked_bar_chart(
-# 	change_type, c_types, 
-# 	stable_type = ['sector', 'Unit'], s_type = ['TC','Billion Btu'],
-# 	width = 0.5, xlabel='Year', ylabel=None, title = ""):
-# 	'''
-# 	change_type : type name used to plot stacked graph
-# 	c_types     : energy_types want to stack
-# 	stable_type : type name which are same between msncodes
-# 	s_type      : stable_type values
-# 	width       : width of bar
-# 	xlabel      : xlabel of graph
-# 	ylabel      : ylabel of graph
-# 	'''
-# 	prepare()
-# 	global X
-# 	global tag
-# 	global state
-# 	fig = plt.figure(figsize=(16,10))
-# 	num_of_types = len(c_types)
-# 	data = np.zeros([4, num_of_types, 50])
-# 	for i in range(num_of_types):
-# 		for row in X:
-# 			info = tag[row['MSN']]
-# 			flag = True
-# 			for j in range(len(stable_type)):
-# 				if info[stable_type[j]] != s_type[j]:
-# 					flag = False
-# 					break
-# 			if info[change_type] == c_types[i] and flag == True:
-# 				data[state[row['StateCode']]][i][int(row['Year']) - start_year] += float(row['Data'])
-# 	ind = np.arange(1960, 2010, 1)
-# 	legends = ['Arizona','California','New Mexico', 'Texas']
-# 	last = np.zeros([4, num_of_types, 50])
-# 	for i in range(4):
-# 		sm = np.sum(data[i], axis=0)
-# 		plt.subplot(2,2,i+1)
-# 		plt.bar(ind, data[i][0]*100.0/sm, width)
-# 		last[i][0] = data[i][0]*100.0/sm
-# 		for j in range(1, num_of_types):
-# 			plt.bar(ind, data[i][j]*100.0/sm, width,bottom=last[i][j-1])
-# 			last[i][j] = last[i][j-1]+data[i][j]*100.0/sm
-# 		plt.legend(c_types)
-# #		fig.text(0.30,0.50,legends[i],ha='center')
-# 		plt.title(legends[i], va='bottom')
-# 	plt.suptitle(title+"="+','.join(c_types))
-# 	fig.savefig(fig_path+title+"="+','.join(c_types)+'-percentage.png')
-# #	plt.show()
+def plot_percentage_stacked_bar_chart(
+	change_type, c_types, 
+	stable_type = ['sector', 'Unit'], s_type = ['TC','Billion Btu'],
+	width = 0.5, xlabel='Year', ylabel=None, title = ""):
+	'''
+	change_type : type name used to plot stacked graph
+	c_types     : energy_types want to stack
+	stable_type : type name which are same between msncodes
+	s_type      : stable_type values
+	width       : width of bar
+	xlabel      : xlabel of graph
+	ylabel      : ylabel of graph
+	'''
+	prepare()
+	global X
+	global tag
+	global state
+	fig = plt.figure(figsize=(16,10))
+	num_of_types = len(c_types)
+	data = np.zeros([4, num_of_types, 50])
+	for i in range(num_of_types):
+		for row in X:
+			info = tag[row['MSN']]
+			flag = True
+			for j in range(len(stable_type)):
+				if info[stable_type[j]] != s_type[j]:
+					flag = False
+					break
+			if info[change_type] == c_types[i] and flag == True:
+				data[state[row['StateCode']]][i][int(row['Year']) - start_year] += float(row['Data'])
+	ind = np.arange(1960, 2010, 1)
+	legends = ['Arizona','California','New Mexico', 'Texas']
+	last = np.zeros([4, num_of_types, 50])
+	for i in range(4):
+		sm = np.sum(data[i], axis=0)
+		plt.subplot(2,2,i+1)
+		plt.bar(ind, data[i][0]*100.0/sm, width)
+		last[i][0] = data[i][0]*100.0/sm
+		for j in range(1, num_of_types):
+			plt.bar(ind, data[i][j]*100.0/sm, width,bottom=last[i][j-1])
+			last[i][j] = last[i][j-1]+data[i][j]*100.0/sm
+		plt.legend(c_types)
+#		fig.text(0.30,0.50,legends[i],ha='center')
+		plt.title(legends[i], va='bottom')
+	plt.suptitle(title+"="+','.join(c_types))
+	fig.savefig(fig_path+title+"="+','.join(c_types)+'-percentage.png')
+#	plt.show()
 
 
 def prepare():
